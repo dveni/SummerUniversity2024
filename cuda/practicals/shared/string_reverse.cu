17f8b8f6921093af0189defa6668a59b45338ea9
Daniel Vera Nieto <daveni@protonmail.com>
2024-07-08 14:14:12 +0200

    Update.

--- cuda/practicals/shared/string_reverse.cu
+++ cuda/practicals/shared/string_reverse.cu
@@ -10,7 +10,7 @@ void reverse_string(char* str, int n){
     auto i = threadIdx.x + blockIdx.x*blockDim.x;
 
     printf("i %d\n", int(i));
-    if (i<n+1){
+    if (i<n){
         printf("i < n %d\n", int(i));
         auto aux = str[i];
         str[n-i] = aux;
